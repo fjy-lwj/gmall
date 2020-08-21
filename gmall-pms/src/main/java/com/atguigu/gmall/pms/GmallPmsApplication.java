46b16ac13abade7d48f54349960c5abdb1d72fac
jinyu <1159213392@qq.com>
2020-08-21 11:44:53 +0800

    '创建后台工程'

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall.pms;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@EnableSwagger2
+@MapperScan("")
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GmallPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPmsApplication.class, args);
+    }
+
+}
